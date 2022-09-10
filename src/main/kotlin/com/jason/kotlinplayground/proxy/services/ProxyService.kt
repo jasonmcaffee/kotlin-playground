8dfd19c7f9de98f4ad1d6464afbf98135039e92f
Jason McAffee <jasonlmcaffee@gmail.com>
2022-09-09 19:51:23 -0600

    proxy working as expected.

--- src/main/kotlin/com/jason/kotlinplayground/proxy/services/ProxyService.kt
+++ src/main/kotlin/com/jason/kotlinplayground/proxy/services/ProxyService.kt
@@ -1,24 +1,41 @@
 package com.jason.kotlinplayground.proxy.services
 
+import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
+import org.springframework.http.client.BufferingClientHttpRequestFactory
+import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.stereotype.Service
-import org.springframework.util.MultiValueMap
+import org.springframework.web.client.HttpStatusCodeException
+import org.springframework.web.client.RestTemplate
+import java.net.URI
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-//https://blogs.ashrithgn.com/writing-a-reverse-proxy-service-in-spring-boot/
 @Service
 class ProxyService {
-    fun proxyRequest(urlToProxyTo: String, body: String, method: HttpMethod, request: HttpServletRequest, response: HttpServletResponse){
-        val uri = request.requestURI
-        val headers = HttpHeaders()
-        request.headerNames.toList().forEach {
-            headers.set(it, request.getHeader(it))
+    fun proxyRequest(urlToProxyTo: String, body: String?, method: HttpMethod, request: HttpServletRequest, response: HttpServletResponse): ResponseEntity<String>{
+        val uri = URI(urlToProxyTo)
+        val headers = createHeadersFromRequest(request)
+        val httpEntity = HttpEntity(body, headers)
+        val factory = BufferingClientHttpRequestFactory(SimpleClientHttpRequestFactory())
+        val restTemplate = RestTemplate(factory)
+        return try {
+            val serverResponse = restTemplate.exchange(uri, method, httpEntity, String::class.java)
+            response.setHeader(HttpHeaders.CONTENT_TYPE, serverResponse.headers[HttpHeaders.CONTENT_TYPE]?.firstOrNull())
+            serverResponse
+        }catch(e: HttpStatusCodeException){
+            ResponseEntity.status(e.rawStatusCode)
+                .headers(e.responseHeaders)
+                .body(e.responseBodyAsString)
         }
-        val headerMap = request.headerNames.toList().map { it to request.getHeader(it) }
-        // val multiValueMap: MultiValueMap<String, String> = ArrayListValuedHashMap<String, String>()
-        // HttpHeaders(headerMap)
-
     }
+}
+
+fun createHeadersFromRequest(request: HttpServletRequest) = HttpHeaders().also{ headers ->
+    request.headerNames.toList().forEach{
+        headers.set(it, request.getHeader(it))
+    }
+    headers.remove(HttpHeaders.ACCEPT_ENCODING)
 }
