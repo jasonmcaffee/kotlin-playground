d0c90ba911b77eaeb154aa4c539c1a61acf181d8
Jason McAffee <jasonlmcaffee@gmail.com>
2022-12-12 19:56:04 -0700

    divide and conquer using async

--- src/test/kotlin/com/jason/kotlinplayground/misc/DivideAndConquer.kt
+++ src/test/kotlin/com/jason/kotlinplayground/misc/DivideAndConquer.kt
@@ -1,11 +1,9 @@
 package com.jason.kotlinplayground.misc
 
 import com.jason.kotlinplayground.kotlin.CircuitBreaker
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.junit.jupiter.api.Test
+import java.util.concurrent.atomic.AtomicReference
 import kotlin.concurrent.thread
 
 class DivideAndConquer(
@@ -28,15 +26,24 @@ class DivideAndConquer(
             runBlocking {
                 val threads = mutableListOf<Thread>()
 
+                val atomicException = AtomicReference<Exception>(null)
                 nextBatch.forEach{func ->
                     val t = thread {
-                        func()
+                        try{
+                            func()
+                        }catch (e: Exception){
+                            atomicException.set(e)
+                        }
+
                     }
                     threads.add(t)
                 }
 
                 threads.forEach{ it.join() }
                 nextBatch.clear() //remove the items from the original list
+                if(atomicException.get() != null){
+                    throw atomicException.get()
+                }
                 val timeDiff = 1000 - getMilli()
                 if(timeDiff > 0 && funcList.size > 0){
                     println("sleeping for $timeDiff ms")
@@ -46,7 +53,47 @@ class DivideAndConquer(
         }
         run()
     }
+
+    //https://www.baeldung.com/kotlin/coroutines-waiting-for-multiple-threads
+    fun runUsingAsync(){
+        val nextEndIndex = if(funcList.size - 1 < maxCallsPerSecond) funcList.size - 1 else maxCallsPerSecond
+        val nextBatch = funcList.subList(0, nextEndIndex)
+        if(nextBatch.isEmpty()){ return }
+
+        timeMilli { getMilli ->
+            runBlocking {//use runblocking here so that we don't need to do the entire call chain as suspend functions
+                withContext(coroutineContext){//no need for awaitAll with this approach.
+                    nextBatch.forEach{ func ->
+                        async(Dispatchers.IO) {//you must specify the context here or it will be done synchronously.
+                            println("running func...")
+                            func()
+                            println("done running func.")
+                        }
+                    }
+                }
+                nextBatch.clear() //remove the items from the original list
+                val timeDiff = 1000 - getMilli()
+                if(timeDiff > 0 && funcList.size > 0){
+                    println("sleeping for $timeDiff ms")
+                    delay(timeDiff)
+                }
+            }
+        }
+    }
 }
+
+//alternative approach that works.
+//                val deferreds = mutableListOf<Deferred<*>>()
+//
+//                nextBatch.forEach{ func ->
+//                    val deferred = GlobalScope.async {
+//                        println("running func...")
+//                        func()
+//                        println("done running func.")
+//                    }
+//                    deferreds.add(deferred)
+//                }
+//                deferreds.awaitAll()
 
 class DivideAndConquerTests{
     @Test fun `should divide and conquer`(){
@@ -65,7 +112,7 @@ class DivideAndConquerTests{
             }
         }
         try{
-            divideAndConquer.run()
+            divideAndConquer.runUsingAsync()
         }catch(e: Exception){
             println("exception: ${e.message}")
         }
